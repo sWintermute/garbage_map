59a4cf2a9b8de43c95aa85354c82a63c12a5a07b
Nikolay Bely <belyynikolay@gmail.com>
2021-07-11 13:04:23 +0700

    Add docker, pg, makefile

--- garbage/urls.py
+++ garbage/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', include('map.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
