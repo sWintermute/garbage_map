2edcd63fd12bd68d3ec54d0a049b4ec27c2694f6
petrov_va <petrov_va@landfinance.ru>
2021-07-18 00:35:37 +0700

    Исправлены модели

--- map/admin.py
+++ map/admin.py
@@ -5,12 +5,12 @@ from .models import Customer
 from .models import Unit
 
 class UnitAdmin(admin.ModelAdmin):
-    list_display = ('address', 'lat', 'lon', 'n_mt')
+    list_display = ('district', 'street', 'corpus', 'n_mt')
     list_display_links = ('n_mt',)
-    search_fields = ('n_mt',)
+    search_fields = ('n_mt', 'district','street')
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('district', 'street', 'building', 'corpus', 'lat', 'lon', 'unit')
+    list_display = ('district', 'street', 'building', 'corpus', 'unit')
     list_display_links = ('unit',)
     
 admin.site.register(Customer, CustomerAdmin)
--- map/models.py
+++ map/models.py
@@ -8,10 +8,24 @@ class Unit(models.Model):
     lon = models.DecimalField(max_digits=9, decimal_places=6, verbose_name='Долгота')
 
     n_mt = models.IntegerField(verbose_name='Идентификатор в MT', unique=True)
-    address = models.CharField(verbose_name='Адрес контейнерной площадки', max_length=200, blank=True, null=True)
+    
+    class District(models.TextChoices):
+        Заводской = 'Заводской'
+        Центральный = 'Центральный'
+        Куйбышевский = 'Куйбышевский'
+        Новоильинский = 'Новоильинский'
+        Орджоникидзевский = 'Орджоникидзевский'
+        Кузнецкий = 'Кузнецкий'    
+
+        __empty__ = "Выберите район"
+        
+    district = models.CharField(choices=District.choices, default=District.__empty__, null=True, verbose_name='Район города', max_length=100)
+    street = models.CharField(verbose_name='Улица', max_length=100, blank=True, null=True)
+    building = models.IntegerField(verbose_name='Номер дома', blank=True, null=True)
+    corpus = models.CharField(verbose_name='Корпус', max_length=3, blank=True, null=True)
 
     def __str__(self):
-        return str(self.address + " " + "№" +str(self.n_mt))
+        return str(self.street) + ", " + str(self.building) + "|" + "#" + str(self.n_mt)
 
     class Meta:
         verbose_name_plural = "Контейнерные площадки"
