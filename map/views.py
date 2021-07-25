7265017fa72176d9ed782f3f28cd76bdcc57ba32
petrov_va <petrov_va@landfinance.ru>
2021-07-25 14:42:27 +0700

    remove debug message

--- map/views.py
+++ map/views.py
@@ -7,7 +7,6 @@ from django.http import HttpResponse
 
 def district_all(request, district):
     units = Unit.objects.filter(district=district)
-    print(len(units), "#################################################################################################################################")
     
     figure = folium.Figure()
     map = folium.Map(location=[53.761683, 87.125225], zoom_start=13)
