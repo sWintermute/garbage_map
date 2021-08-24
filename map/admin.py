88313136f9a157cc75f06455ebcf2b9fe7cbeefa
Vladimir Petrov <petrov_va@landfinance.ru>
2021-08-24 15:07:49 +0700

    reformat style

--- map/admin.py
+++ map/admin.py
@@ -4,14 +4,17 @@ from django.contrib.admin.decorators import register
 from .models import Customer
 from .models import Unit
 
+
 class UnitAdmin(admin.ModelAdmin):
-    list_display = ('district', 'address', 'n_mt')
-    list_display_links = ('n_mt',)
-    search_fields = ('n_mt', 'district','address')
+    list_display = ("district", "address", "n_mt")
+    list_display_links = ("n_mt",)
+    search_fields = ("n_mt", "district", "address")
+
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('district', 'street', 'building', 'corpus', 'unit')
-    list_display_links = ('unit',)
-    
+    list_display = ("district", "street", "building", "corpus", "unit")
+    list_display_links = ("unit",)
+
+
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Unit, UnitAdmin)
