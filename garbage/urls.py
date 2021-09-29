f163ffebe88f310d88f1f799a21aa7974b10fff4
Vladimir Petrov <petrov_va@landfinance.ru>
2021-09-29 12:11:20 +0700

    push

--- garbage/urls.py
+++ garbage/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("", include("map.urls")),
     path("admin/", admin.site.urls),
-    path("map/", include("map.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- map/urls.py
+++ map/urls.py
@@ -4,7 +4,7 @@ from django.urls.resolvers import URLPattern
 from .views import district_all, unit, clear_map
 
 urlpatterns = [
-    path("district/<str:district>", district_all),
-    path("<int:n_mt>", unit),
+    path('district/<str:district>', district_all),
+    path('<int:n_mt>', unit),
     path("", clear_map),
 ]
